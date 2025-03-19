bc8f89e13ba1637268654e24b742eaef00d35863
peu77 <emilebert@icloud.com>
2025-03-19 17:45:22 +0100

    add Fixed class and color definitions for improved number handling and output formatting

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,133 @@
+#include "Fixed.h"
+#include <iostream>
+
+Fixed::Fixed() : _value(0) {
+}
+
+Fixed::Fixed(const int value) {
+	_value = value << _fractionalBits;
+}
+
+Fixed::Fixed(const float value) {
+	_value = roundf(value * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+	*this = fixed;
+}
+
+Fixed::~Fixed() {
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
+	out << fixed.toFloat();
+	return out;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+	_value = fixed.getRawBits();
+	return *this;
+}
+
+Fixed Fixed::operator>(const Fixed &fixed) const {
+	return _value > fixed.getRawBits();
+}
+
+Fixed Fixed::operator<(const Fixed &fixed) const {
+	return _value < fixed.getRawBits();
+}
+
+Fixed Fixed::operator>=(const Fixed &fixed) const {
+	return _value >= fixed.getRawBits();
+}
+
+Fixed Fixed::operator<=(const Fixed &fixed) const {
+	return _value <= fixed.getRawBits();
+}
+
+Fixed Fixed::operator==(const Fixed &fixed) const {
+	return _value == fixed.getRawBits();
+}
+
+Fixed Fixed::operator!=(const Fixed &fixed) const {
+	return _value != fixed.getRawBits();
+}
+
+Fixed Fixed::operator+(const Fixed &fixed) const {
+	return Fixed(toFloat() + fixed.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed &fixed) const {
+	return Fixed(toFloat() - fixed.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed &fixed) const {
+	return Fixed(toFloat() * fixed.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed &fixed) const {
+	return Fixed(toFloat() / fixed.toFloat());
+}
+
+Fixed &Fixed::operator++(void) {
+	_value++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int) {
+	Fixed tmp(*this);
+	operator++();
+	return tmp;
+}
+
+Fixed &Fixed::operator--(void) {
+	_value--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int) {
+	Fixed tmp(*this);
+	operator--();
+	return tmp;
+}
+
+Fixed &Fixed::min(Fixed &a, Fixed &b) {
+	if (a.toFloat() < b.toFloat())
+		return a;
+	return b;
+}
+
+Fixed &Fixed::max(Fixed &a, Fixed &b) {
+	if (a.toFloat() > b.toFloat())
+		return a;
+	return b;
+}
+
+const Fixed &Fixed::min(Fixed const &a, Fixed const &b) {
+	if (a.toFloat() < b.toFloat())
+		return a;
+	return b;
+}
+
+const Fixed &Fixed::max(Fixed const &a, Fixed const &b) {
+	if (a.toFloat() > b.toFloat())
+		return a;
+	return b;
+}
+
+int Fixed::getRawBits(void) const {
+	return _value;
+}
+
+void Fixed::setRawBits(int const raw) {
+	_value = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return static_cast<float>(_value) / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const {
+	return _value >> _fractionalBits;
+}
+
---
+++ ex02/Fixed.h
@@ -0,0 +1,51 @@
+#ifndef FIXED_H
+#define FIXED_H
+#include <ostream>
+
+
+class Fixed {
+private:
+	int _value{};
+	static const int _fractionalBits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &fixed);
+	Fixed(const int  value);
+	Fixed(const float value);
+	~Fixed();
+
+	friend std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+	Fixed &operator=(const Fixed &fixed);
+	Fixed operator>(const Fixed &fixed) const;
+	Fixed operator<(const Fixed &fixed) const;
+	Fixed operator>=(const Fixed &fixed) const;
+	Fixed operator<=(const Fixed &fixed) const;
+	Fixed operator==(const Fixed &fixed) const;
+	Fixed operator!=(const Fixed &fixed) const;
+
+	Fixed operator+(const Fixed &fixed) const;
+	Fixed operator-(const Fixed &fixed) const;
+	Fixed operator*(const Fixed &fixed) const;
+	Fixed operator/(const Fixed &fixed) const;
+
+	Fixed &operator++(void);
+	Fixed operator++(int);
+	Fixed &operator--(void);
+	Fixed operator--(int);
+
+	static Fixed &min(Fixed &a, Fixed &b);
+	static Fixed &max(Fixed &a, Fixed &b);
+	static const Fixed &min(Fixed const &a, Fixed const &b);
+	static const Fixed &max(Fixed const &a, Fixed const &b);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+	float toFloat(void) const;
+	int toInt(void) const;
+
+};
+
+
+#endif //FIXED_H
